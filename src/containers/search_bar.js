5f7e28276d5f59a6c8c26d804ba3e3b5930ac513
Qasim Laghari <go.jets.laghari@gmail.com>
2016-04-09 18:48:44 -0500

    Add Weather Reducer to handle fetchWeather action

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index'
 
 export default class SearchBar extends Component {
   constructor(props){
@@ -7,6 +10,7 @@ export default class SearchBar extends Component {
     this.state = { term: ''};
 
     this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   onInputChange(event) {
@@ -18,6 +22,12 @@ export default class SearchBar extends Component {
 
   onFormSubmit(event) {
     event.preventDefault();
+
+    //Let's go and fetch the data using an action creator
+    this.props.fetchWeather(this.state.term);
+    this.setState({
+      term: ''
+    });
   }
 
   render() {
@@ -37,3 +47,9 @@ export default class SearchBar extends Component {
     );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators( {fetchWeather}, dispatch);
+}
+
+export default connect (null, mapDispatchToProps)(SearchBar);
